0c742ae972f9d3997b36bd3bbb15d7a85a78625e
Kedar Iyer <kedarmail@gmail.com>
2017-07-06 17:46:47 -0400

    sigma point generation

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,29 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size()) {
+    cout << "Error: estimations size error" << endl;
+  }
+
+  int rmse_length = estimations[0].size();
+  VectorXd rmse = VectorXd(rmse_length);
+  
+  //accumulate squared residuals
+  VectorXd residual_sum(rmse_length);
+  residual_sum.fill(0);
+  for(int i=0; i < estimations.size(); ++i) {
+	  VectorXd diff = ground_truth[i] - estimations[i];
+      cout << diff << endl;
+	  residual_sum += diff.cwiseProduct(diff);
+  }
+
+  //calculate the mean
+  VectorXd mean(rmse_length);
+  mean = 1.0 / rmse_length * residual_sum;
+
+  //calculate the squared root
+  rmse = mean.cwiseSqrt();
+
+  return rmse;
 }
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -15,7 +15,7 @@ UKF::UKF() {
   use_laser_ = true;
 
   // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
+  use_radar_ = false;
 
   // initial state vector
   x_ = VectorXd(5);
@@ -24,10 +24,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 15;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -51,6 +51,20 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  bool is_initialized_ = false;
+
+  n_x_ = 5;
+  n_aug_ = 7;
+
+  // Intialize states
+  x_ << 0,0,2,M_PI/2,0;
+  P_ << MatrixXd::Identity(n_x_,n_x_);
+  P_(0,0) = std_laspx_*std_laspx_;
+  P_(1,1) = std_laspy_*std_laspy_;
+  
+  lambda_ = 3 - n_aug_;
+  Xsig_pred_ = MatrixXd(2*n_aug_ + 1, n_aug_);
+  weights_ = VectorXd(2*n_aug_ + 1);
 }
 
 UKF::~UKF() {}
@@ -66,6 +80,38 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (!is_initialized_) {
+    cout << "First Measurement" << endl;
+    time_us_ = meas_package.timestamp_;
+    
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+      cout << "Initial Radar" << endl;
+      float rho = meas_package.raw_measurements_[0];
+      float phi = meas_package.raw_measurements_[1];
+      x_(1) = rho * cos(phi);
+      x_(2) = rho * sin(phi);
+    }
+
+    else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+        cout << "Initial Laser" << endl;
+        float px = meas_package.raw_measurements_[0];
+        float py = meas_package.raw_measurements_[1];
+        x_(0) = px;
+        x_(1) = py;
+    }
+
+    cout << meas_package.raw_measurements_ << endl;
+    is_initialized_ = true;
+    return;
+  }
+
+  //////////////////
+  // Prediction
+
+  Prediction();
+
+  
+
 }
 
 /**
@@ -80,6 +126,36 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  // time delta
+  float dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+  time_us_ = meas_package.timestamp_;
+  
+  // Augmented states
+  VectorXd x_aug = VectorXd(n_aug_);
+  MatrixXd X_sig_aug = MatrixXd(2*n_aug_ + 1, n_aug_);
+  MatrixXd P_aug = MatrixXd(n_aug, n_aug);
+
+  x_aug.fill(0.0);
+
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(n_x, n_x_) = P_;
+  P_aug(n_x_,n_x_) = std_a_ * std_a_;
+  P_aug(n_x_+1, n_x_+1) = std_yawdd_, std_yawdd_;
+
+  // square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+    
+  // create augmented sigma points
+  MatrixXd X2 = sqrt(3) * A;
+  X2.colwise() += x_aug;
+  MatrixXd X3 = -sqrt(3) * A;
+  X3.colwise() += x_aug;
+  Xsig_aug << x_aug, X2, X3;
+  
+  // predict sigma point motion
+
+
 }
 
 /**
